a58c9e067cc196806b5063a5b053171984d2763e
dheerajpaldeveloper <dheerajpaldeveloper@gmail.com>
2025-04-24 21:17:01 +0530

    header and sidebar added

--- app/(dashboard)/components/sidebar.tsx
+++ app/(dashboard)/components/sidebar.tsx
@@ -1,8 +1,64 @@
+import Link from 'next/link';
 import React from 'react'
+import * as Icons from "react-icons/fa";
+
 
 function SideBar() {
+
+    const menuItems = [
+        {
+          title: "Admin Portal",
+          items: [
+            {
+              icon: <Icons.FaHome /> ,
+              label: "Dashboard",
+              href: "/",
+            },
+            {
+              icon: <Icons.FaUsers />,
+              label: "Users",
+              href: "/list/teachers",
+            },
+            {
+              icon: <Icons.FaAccessibleIcon />,
+              label: "Support",
+              href: "/list/students",
+            },
+            
+            {
+              icon: <Icons.FaWhmcs />,
+              label: "Settings",
+              href: "/settings",
+            },
+           
+          ],
+        }
+      ];
   return (
-    <div>SideBar</div>
+    <div className="mt-3 text-sm">
+    {menuItems.map((i) => (
+      <div className="flex flex-col gap-2" key={i.title}>
+        <span className="hidden lg:block text-black  my-4 text-4xl font-medium
+ ">
+          {i.title}
+        </span>
+        {i.items.map((item) => {
+          if (item) {
+            return (
+              <Link
+                href={item.href}
+                key={item.label}
+                className="flex items-center justify-center lg:justify-start gap-4 text-gray text-xl py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+              >
+                {item.icon}
+                <span className="hidden lg:block">{item.label}</span>
+              </Link>
+            );
+          }
+        })}
+      </div>
+    ))}
+  </div>
   )
 }
 
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -20,26 +20,38 @@ export default async function DashboardLayout({
   }
 
   return (
-    <html >
-      <body>
-        <div  className='flex gap-2 m-2'>
 
-          <div className="border-2">
-            <SideBar />
-          </div>
+    <div className="h-screen flex">
+    {/* LEFT */}
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-3">
+  
+      <SideBar />
+    </div>
+    {/* RIGHT */}
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <Header />
+      {children}
+    </div>
+  </div>
 
-          <div >
-            <div className="border-2" ><Header  /></div>
+    //
+   
+        // <div  className='flex gap-2 m-2'>
+
+        //   <div className="border-2">
+        //     <SideBar />
+        //   </div>
+
+        //   <div >
+        //     <div className="border-2" ><Header  /></div>
             
-            <div className="border-2" >
-              {children}
-            </div>
-          </div>
+        //     <div className="border-2" >
+        //       {children}
+        //     </div>
+        //   </div>
 
-        </div>
+        // </div>
         
-        
-      </body>
-    </html>
+
   );
 }
